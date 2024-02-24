f09966c81a706cad5173096083997e8b0da4167a
vamsi-kanakam <vamsikanakam2002@gmail.com>
2024-02-24 13:33:50 -0600

    Task 1 Added.

---
+++ Task-1/StarterProject/StarterProject/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarterProject.Models;
+namespace StarterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //TaxCalculator tc = new TaxCalculator { Subtotal = 100, TaxRate = 0.10M };
+            TaxCalculator tc = new TaxCalculator();
+            tc.Subtotal = 100;
+            tc.TaxRate = 0.10M;
+
+            return View(tc);
+        }
+    }
+}
---
+++ Task-1/StarterProject/StarterProject/Models/TaxCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace StarterProject.Models
+{
+    public class TaxCalculator
+    {
+        public decimal Subtotal { get; set; }
+        public decimal TaxRate { get; set; }
+
+        public decimal CalculateTotal()
+        {
+            return Subtotal * (1 + TaxRate);
+        }
+    }
+}
