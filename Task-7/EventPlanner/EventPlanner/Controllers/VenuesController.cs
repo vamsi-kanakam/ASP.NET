29a8d0966dd9f0784d19b839d8100b8bbc832551
vamsi-kanakam <vamsikanakam2002@gmail.com>
2024-03-13 14:28:21 -0500

    Task-7 Added.

---
+++ Task-7/EventPlanner/EventPlanner/Controllers/VenuesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventPlanner.DAL;
+using EventPlanner.Models;
+
+namespace EventPlanner.Controllers
+{
+    public class VenuesController : Controller
+    {
+        private readonly EventPlannerContext _context;
+
+        public VenuesController(EventPlannerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Venues
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Venues.ToListAsync());
+        }
+
+        // GET: Venues/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Venues == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues
+                .FirstOrDefaultAsync(m => m.VenueId == id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+
+            return View(venue);
+        }
+
+        // GET: Venues/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Venues/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VenueId,Name,Address,MaxCapacity")] Venue venue)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venue);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(venue);
+        }
+
+        // GET: Venues/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Venues == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+            return View(venue);
+        }
+
+        // POST: Venues/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VenueId,Name,Address,MaxCapacity")] Venue venue)
+        {
+            if (id != venue.VenueId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venue);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VenueExists(venue.VenueId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(venue);
+        }
+
+        // GET: Venues/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Venues == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues
+                .FirstOrDefaultAsync(m => m.VenueId == id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+
+            return View(venue);
+        }
+
+        // POST: Venues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Venues == null)
+            {
+                return Problem("Entity set 'EventPlannerContext.Venues'  is null.");
+            }
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue != null)
+            {
+                _context.Venues.Remove(venue);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VenueExists(int id)
+        {
+          return _context.Venues.Any(e => e.VenueId == id);
+        }
+    }
+}
